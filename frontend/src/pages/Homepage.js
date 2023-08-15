bcbf090610739f0269595cc10440614139119d1d
krishaDas <ps299106@gmail.com>
2023-08-15 09:27:23 +0530

    first commit

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Grid, Box, Button } from '@mui/material';
+import styled from 'styled-components';
+import Students from "../assets/front.png";
+import { LightPurpleButton } from '../components/buttonStyles';
+
+const Homepage = () => {
+    return (
+        <StyledContainer>
+            <Grid container spacing={0}>
+                <Grid item xs={12} md={6}>
+                    <StyledImage src={Students} alt="students" />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <StyledPaper elevation={3}>
+                        <StyledTitle>
+                            Welcome to School Management System
+                        </StyledTitle>
+                        <StyledText>
+                            Simplify school management, organize classes, and manage students and faculty.
+                            Track attendance, evaluate performance, and offer feedback effortlessly.
+                            Access records, view grades, and communicate seamlessly.
+                        </StyledText>
+                        <StyledBox>
+                            <StyledLink to="/choose">
+                                <LightPurpleButton variant="contained" fullWidth>
+                                    Login
+                                </LightPurpleButton>
+                            </StyledLink>
+                            <StyledLink to="/chooseasguest">
+                                <Button variant="outlined" fullWidth
+                                    sx={{ mt: 2, mb: 1, color: "#7f56da", borderColor: "#7f56da" }}
+                                >
+                                    Login as Guest
+                                </Button>
+                            </StyledLink>
+                            <StyledText>
+                                Don't have an account?{' '}
+                                <Link to="/Adminregister" style={{ color: "#550080" }}>
+                                    Sign up
+                                </Link>
+                            </StyledText>
+                        </StyledBox>
+                    </StyledPaper>
+                </Grid>
+            </Grid>
+        </StyledContainer>
+    );
+};
+
+export default Homepage;
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const StyledPaper = styled.div`
+  padding: 24px;
+  height: 100vh;
+`;
+
+const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  padding: 24px;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 2.5rem;
+  color: #252525;
+  font-weight: bold;
+  padding-top: 0;
+  letter-spacing: normal;
+  line-height: normal;
+`;
+
+const StyledText = styled.p`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  margin-top: 45px;
+  margin-left: 0px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
